8fc5a0ff65d566a3bc4e850ac332ae2c7f120002
noobgrind <168707521+noobgrind@users.noreply.github.com>
2024-11-10 22:17:48 +0530

    Update square.c

--- square.c
+++ square.c
@@ -2,9 +2,9 @@
 #include<stdio.h>
 #include "testJni.h" 
 
-JNIEXPORT jint JNICALL Java_testJni_sqr(JNIEnv *e, jobject o, jint a,  jint b)
+JNIEXPORT jint JNICALL Java_testJni_sqr(JNIEnv *e, jobject o, jint a)
 {
 	jint res;
-	res = a * b;
+	res = a * a;
 	return res;
 }
